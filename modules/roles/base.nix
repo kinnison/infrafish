4c5d1784d8fa024a524a02779dd29bd09f5ca1a3
Daniel Silverstone <dsilvers@digital-scurf.org>
2023-06-04 16:27:19 +0100

    misc: Move SSH keys up
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- modules/roles/base.nix
+++ modules/roles/base.nix
@@ -1,6 +1,6 @@
 # Pepperfish base role
 
-{ config, pkgs, lib, nodeName, ... }:
+{ config, pkgs, lib, nodeName, ppfmisc, ... }:
 
 with lib;
 
@@ -19,9 +19,7 @@ with lib;
     networking.hostName = nodeName;
     networking.domain = "";
 
-    users.users.root.openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOgi5G6r/21IH5p0gDWQPomQPRcyGYtVK6D3uIl+A1QMvT6g9M6D+ecjvtQ8e1Rx5vGI2vgWxLN9fwIuoeWTRE9uRxD0fy6OHgSF95XY82OFC3RK1Bc3jLAVMP/BZUCqyXYbvgp6ggsc2fgEi+h+ZPG5bXwGgbwz0+vUJjMWZJKq0DVbKuVf41sOttBJ6FFZ7VbQ4t6qrq5HEnfXUPdCWtlfc+kDKwy+QLCB/xXBfkMzOsXrFfUdrx/80rWlaGa+0BuzHYst7xyP38KytEqHoz7txMv4HiLf2fkOcfpgIlFi+KPEcWfIUSgCKfC/7lyb2LHNec5IG/HYL59a2TCmIl cardno:5407828"
-    ];
+    users.users.root.openssh.authorizedKeys.keys = ppfmisc.rootPermittedKeys;
 
     users.extraUsers.root.shell =
       mkOverride 50 "${pkgs.bashInteractive}/bin/bash";
