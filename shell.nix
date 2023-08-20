e22903094de67a4c55ce80ba47b71f137123e897
Daniel Silverstone <dsilvers@digital-scurf.org>
2023-08-20 22:00:55 +0100

    core: Update mailconfig flake and add httpie
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
-{ mkShell, sops, deploy-rs, nixpkgs-fmt, wireguard-tools, age, ssh-to-age }:
+{ mkShell, sops, deploy-rs, nixpkgs-fmt, wireguard-tools, age, ssh-to-age
+, httpie }:
 
 mkShell {
   nativeBuildInputs =
-    [ age ssh-to-age sops deploy-rs nixpkgs-fmt wireguard-tools ];
+    [ age ssh-to-age sops deploy-rs nixpkgs-fmt wireguard-tools httpie ];
 }
