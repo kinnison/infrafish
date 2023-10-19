f81d4040fefe0b21bc0c25b2dd872fca13b77c53
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2023-10-19 13:56:46 +0100

    users: Add rauha key to me, and add Lesley

--- hosts/shell/users.nix
+++ hosts/shell/users.nix
@@ -4,6 +4,7 @@
     email = "dsilvers@digital-scurf.org";
     defaultKeys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOgi5G6r/21IH5p0gDWQPomQPRcyGYtVK6D3uIl+A1QMvT6g9M6D+ecjvtQ8e1Rx5vGI2vgWxLN9fwIuoeWTRE9uRxD0fy6OHgSF95XY82OFC3RK1Bc3jLAVMP/BZUCqyXYbvgp6ggsc2fgEi+h+ZPG5bXwGgbwz0+vUJjMWZJKq0DVbKuVf41sOttBJ6FFZ7VbQ4t6qrq5HEnfXUPdCWtlfc+kDKwy+QLCB/xXBfkMzOsXrFfUdrx/80rWlaGa+0BuzHYst7xyP38KytEqHoz7txMv4HiLf2fkOcfpgIlFi+KPEcWfIUSgCKfC/7lyb2LHNec5IG/HYL59a2TCmIl cardno:5407828"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJQNkNYNGj+O//LldKl9wSjIrrtgI79nIArijEGNRgM danielsilverstone@rauha"
     ];
     shell = "zsh";
     websites = { "www.digital-scurf.org" = { listings = true; }; };
@@ -16,5 +17,13 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDgk5rUMqzf+I8lfBxIvr21mgcTSLB47bxKCPkH2AS3o2mLIdGgocP/LWHT5thM3yZkvucAJoP+mPyTSFDhIrFYTWoLc7zaebYoOJ8V9ZmyV0vcxZZssjx+VkKMYHAkLKw0u+7LOeUqizuE7WApqre5RO5YPIcZAGkQgObPCTNh3enL5tjxWr+5rApBAXhAf7YrfZgS/zs0RyxQ0bOaaTgHkjAcJ96EiY3oB3PaICz+KYxE/6Hwt56bu0MWdO3iLoKDOT7pjDTNkFyWVhB3H97t/gEShJtio1/QuPpBrOiLEx+bauUS4MpgJvG72S5yHU8AOjUGyh15ibMcM8f/X2Pe8rIsdtCIjPd4Y4rUBnmjdGm7uZOXszbatzm6ugnfJUPffgQfwyKyizHOdvaYxr8Mri6Evt8f7PYiTI/0B7kHxpT+/FBY2WAhwst67bzLgSZC6ohNFtSF5QlMvjuzRe/0i/POG9CYHBUlWPxb9qDgn06G9sPeJh26qFItIMMMdv8= rjek@cliche"
     ];
     shell = "bash";
+  };
+
+  dkscully = {
+    name = "Lesley Mitchell";
+    email = "dkscully@geah.org";
+    defaultKeys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCt+rzeHl8fYF5wX0p3MOiJWRvMTOluJh8n/r0qLfPyWdYq6z4pL+DlKOjO3KiZw0HjgkCMmk2E847aslMMTx0E113cWBqPZ0uP5lgfG5WrkZ1vMXRmy/k1itBd5FET5YQaB0lReoXk60wr2v9F41v8bG3RWEuZ9NbK4nqQjjIZKFmS04Y+NYtdUxtBaOp7hSTdbwHD1hC7j5Y+1Bucxi8DoLMUdk6E6kuvJST62X2tV8JlqFgukPgVOX+QXnIeqom51IcSvTuI+fLG0O6WtZhBw7wKG9uf5ye3Px5P9TQjU0Ejp3UJGdksUak3WCqTCyRGT0w/hpVY6THxSo87f5Jt dkscully@hex.geah.org"
+    ];
   };
 }
